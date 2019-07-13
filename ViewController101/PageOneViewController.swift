60008785a8decb5b84ee929ac5ce97d97cb177af
AphiwitSCB <aphiwitscbsmacbook@AphiwitSCBs-MacBook-Pro.local>
2019-07-13 15:12:51 +0700

    mainview with PageOneController

---
+++ ViewController101/PageOneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PageOneViewController.swift
+//  ViewController101
+//
+//  Created by AphiwitSCB on 13/7/2562 BE.
+//  Copyright © 2562 OnceKB. All rights reserved.
+//
+
+import UIKit
+
+class PageOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
